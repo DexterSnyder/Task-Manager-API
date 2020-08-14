649d760da315f20ee06e85ba682fd3c50dfe7708
DexterSnyder <40254136+DexterSnyder@users.noreply.github.com>
2020-08-13 19:15:13 -0600

    uploading after udemy course

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const multer = require('multer')
-const sharp = require('sharp')
+    //const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
-router.post('/users', async (req, res) => {
+router.post('/users', async(req, res) => {
     const user = new User(req.body)
 
     try {
@@ -19,7 +19,7 @@ router.post('/users', async (req, res) => {
     }
 })
 
-router.post('/users/login', async (req, res) => {
+router.post('/users/login', async(req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
@@ -29,9 +29,9 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.post('/users/logout', auth, async (req, res) => {
+router.post('/users/logout', auth, async(req, res) => {
     try {
-        req.user.tokens = req.user.tokens.filter((token) => {
+        req.user.tokens = req.user.tokens.filter(token => {
             return token.token !== req.token
         })
         await req.user.save()
@@ -42,7 +42,7 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
-router.post('/users/logoutAll', auth, async (req, res) => {
+router.post('/users/logoutAll', auth, async(req, res) => {
     try {
         req.user.tokens = []
         await req.user.save()
@@ -52,21 +52,21 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 })
 
-router.get('/users/me', auth, async (req, res) => {
+router.get('/users/me', auth, async(req, res) => {
     res.send(req.user)
 })
 
-router.patch('/users/me', auth, async (req, res) => {
+router.patch('/users/me', auth, async(req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
 
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
 
     try {
-        updates.forEach((update) => req.user[update] = req.body[update])
+        updates.forEach(update => (req.user[update] = req.body[update]))
         await req.user.save()
         res.send(req.user)
     } catch (e) {
@@ -74,7 +74,7 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
-router.delete('/users/me', auth, async (req, res) => {
+router.delete('/users/me', auth, async(req, res) => {
     try {
         await req.user.remove()
         sendCancelationEmail(req.user.email, req.user.name)
@@ -97,22 +97,31 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-    req.user.avatar = buffer
-    await req.user.save()
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+router.post(
+    '/users/me/avatar',
+    auth,
+    upload.single('avatar'),
+    async(req, res) => {
+        // const buffer = await sharp(req.file.buffer)
+        //     .resize({ width: 250, height: 250 })
+        //     .png()
+        //     .toBuffer()
+        // req.user.avatar = req.file.buffer
+        // await req.user.save()
+        res.send(200)
+    },
+    (error, req, res, next) => {
+        res.status(400).send({ error: error.message })
+    }
+)
 
-router.delete('/users/me/avatar', auth, async (req, res) => {
+router.delete('/users/me/avatar', auth, async(req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
 })
 
-router.get('/users/:id/avatar', async (req, res) => {
+router.get('/users/:id/avatar', async(req, res) => {
     try {
         const user = await User.findById(req.params.id)
 
---
+++ tests/task.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest')
+const Task = require('../src/models/task')
+const app = require('../src/app')
+const { userOneId, userOne, setupDatabase, userTwoId, userTwo, taskOne } = require('./fixtures/db')
+
+beforeEach(setupDatabase)
+
+test('Should create task for user', async() => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From my test'
+        })
+        .expect(201)
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false)
+})
+
+test('Should get tasks for user one', async() => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should not allow users to delete others tasks', async() => {
+    const response = await request(app)
+        .delete(`/task/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
